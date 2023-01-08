e294a1b5cb288b8f5f6b464074edb6dd40235441
Ehan and Ayan <Ehan and Ayan@DESKTOP-1RSV0OU>
2023-01-07 19:00:05 -0800

    Add project files.

---
+++ PythonGuessingGame/PythonGuessingGame.py
@@ -0,0 +1,54 @@
+
+# The python version of guessing game
+# 
+#
+
+import random
+
+randomnumber = random.randint(0,100)
+inputnumber = 0
+guesses = 0
+notplaying = False
+
+print("Welcome to Guessing Game (Python edition)")
+print("Your goal is to guess a number between 1-100.")
+print("Good luck!")
+print("---------------------------------------------")
+
+while notplaying != True:
+    
+    if guesses > 5:
+        print("You lose....")
+        print("Play again? (Y/N): ")
+        again = input()
+        if again == 'Y':
+            randomnumber = random.randint(0,100)
+            guesses = 0
+            continue
+        else:
+            notplaying = True
+            print("Thanks for playing!")
+
+    print("Guess a number: ")
+    inputnumber = int(input())
+    guesses += 1
+
+    if inputnumber < randomnumber:
+        print("Too low!")
+    elif inputnumber > randomnumber:
+        print("Too high!")
+    else:
+        print("You got it! You win!")
+        print("It took you " + str(guesses) + " guesses!")
+        print("Play again? (Y/N): ")
+        again = input()
+        if again == 'Y':
+            randomnumber = random.randint(0,100)
+            guesses = 0
+            continue
+        else:
+            notplaying = True
+            print("Thank you for playing!")
+
+
+
